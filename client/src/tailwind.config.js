7dba505a66fb88390a79ff6415bc8f13467cae87
unknown <Win10@20GB-2SGB-AND.cmdo.policiamilitar.sp.gov.br>
2026-04-10 16:16:29 -0300

    Ativando o design do Tailwind

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
-  "./index.html",
-  "./client/src/**/*.{js,ts,jsx,tsx}",
-],
+  content: [
+    "./index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}", // Isso aqui é o segredo!
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      }
+    },
   },
   plugins: [],
 }
