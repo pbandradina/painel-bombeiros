66026add3752da244740bb4686ac1dca7424b037
pbandradina <pbandradina@gmail.com>
2026-04-22 13:16:53 -0400

    fix: Usar api.js como serverless function do Vercel

---
+++ api.js
@@ -0,0 +1,21 @@
+import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./api/routers.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rota oficial da API que o seu Frontend chama
+app.use(
+  "/api/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => ({}),
+  })
+);
+
+// Exportar para a Vercel usar como Serverless Function
+export default app;
