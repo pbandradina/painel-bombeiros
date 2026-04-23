d3ad287e5ca91412114549af51303dcf35dadbc2
pbandradina <pbandradina@gmail.com>
2026-04-23 07:49:48 -0400

    Remover tRPC e usar apenas React Query com cliente HTTP

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import superjson from "superjson";
 import App from "./App";
-import { trpc } from "./lib/trpc";
 import "./index.css";
 
 // Criar QueryClient
@@ -16,27 +13,9 @@ const queryClient = new QueryClient({
   },
 });
 
-// Criar tRPC Client
-const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: "https://gntlcxaoxtzukaizoxoi.supabase.co/functions/v1/api/trpc",
-      transformer: superjson as any,
-      fetch(input, init) {
-        return globalThis.fetch(input, {
-          ...(init ?? {}),
-          credentials: "omit",
-        });
-      },
-    }),
-  ],
-});
-
 // Renderizar app com providers
 createRoot(document.getElementById("root")!).render(
-  <trpc.Provider client={trpcClient} queryClient={queryClient}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </trpc.Provider>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
